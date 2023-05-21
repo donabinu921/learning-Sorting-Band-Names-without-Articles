219ab102449324aafc798300ea6b68a1f15a82a5
donabinu921 <elizabethdona921@gmail.com>
2023-05-21 16:25:53 +0530

    Bands sorted

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
 function strip(bandName) {
     return bandName.replace(/^(a |the |an )/i, '').trim();
   }
-  
+
+  const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
